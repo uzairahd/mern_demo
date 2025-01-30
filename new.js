1c40eb0b509ba2f1109b0b1b621361aed525a077
uzairahd <uzairahmedzm1@gmail.com>
2025-01-30 08:58:10 +0530

    portfoliio

---
+++ new.js
@@ -0,0 +1,14 @@
+let n=99
+let ans=(n%2==0)?":Even":"Odd"
+console.log(ans)
+
+let mark=60
+let grade=(mark>=90)?"A":(mark>70)?"B":(mark>40)?"C":"F"
+console.log(grade)
+
+let arr=[1,2,3,4,5,6]
+let arr2=[7,8,9]
+let arrMerge=[...arr,...arr2]
+console.log(arrMerge)
+
+let user={name:"varun"}
